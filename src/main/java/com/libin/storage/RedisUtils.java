70da56b8b5cf7278b322d6ec385045c9df15e27a
baolibin <2578858653@qq.com>
2018-12-25 21:17:08 +0800

    update

---
+++ src/main/java/com/libin/storage/RedisUtils.java
@@ -0,0 +1,10 @@
+package com.libin.storage;
+
+/**
+ * Copyright (c) 2018/12/25. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose :
+ */
+public class RedisUtils {
+}
